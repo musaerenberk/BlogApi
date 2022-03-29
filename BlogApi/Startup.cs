21a22359c9e34b285e8f702442d656fa20d64c92
Musa Erenberk Karadeli <73529687+musaerenberk@users.noreply.github.com>
2022-03-29 23:20:45 +0300

    AddScoped added

--- BlogApi/Startup.cs
+++ BlogApi/Startup.cs
@@ -28,7 +28,7 @@ namespace BlogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IBookRepository, BookRepository>();
             services.AddDbContext<BlogContext>(o => o.UseSqlite("Data source=blogs.db"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
